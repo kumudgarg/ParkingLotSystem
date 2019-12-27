51d8840da3a0a4379611ec1075fa6593d2b3d2a1
kumudgarg <kumud50garg@gmail.com>
2019-12-27 15:11:49 +0530

    ADDED : function to park vehicle in parking lot

---
+++ src/main/java/com/parkinglot/ParkingLotSystem.java
@@ -0,0 +1,19 @@
+package com.parkinglot;
+
+public class ParkingLotSystem {
+    private Object vehicle;
+
+    public boolean park(Object vehicle) {
+        this.vehicle = vehicle;
+        return true;
+    }
+
+    public boolean unPark(Object vehicle) {
+        if (this.vehicle.equals(vehicle)) {
+            this.vehicle = null;
+            return true;
+        }
+        return false;
+    }
+
+}
--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -1,4 +1,30 @@
 package com.parkinglot;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class ParkingLotTest {
+    ParkingLotSystem parkingLotSystem;
+    Object vehicle;
+
+    @Before
+    public void setUp() throws Exception {
+        parkingLotSystem = new ParkingLotSystem();
+        vehicle = new Object();
+    }
+
+    @Test
+    public void givenParkingLot_WhenVehicleParked_shouldReturnTrue() {
+        boolean isVehicleParked = parkingLotSystem.park(new Object());
+        Assert.assertTrue(isVehicleParked);
+    }
+
+    @Test
+    public void givenParkingLot_WhenVehicleParked_shouldUnparked() {
+        parkingLotSystem.park(vehicle);
+        boolean isVehicleParked = parkingLotSystem.unPark(vehicle);
+        Assert.assertTrue(isVehicleParked);
+    }
+
 }
